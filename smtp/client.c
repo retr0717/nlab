4eb549db545570b4fa5cd7ae153424835837337c
retr0717 <alwinaji717@gmail.com>
2025-03-16 13:48:37 +0530

    smtp fix

--- smtp/client.c
+++ smtp/client.c
@@ -5,169 +5,134 @@
 #include <netinet/in.h>
 #include <unistd.h>
 
-int main()
-{
-	char buff[100];
-	int k, sock_desc;
-	struct sockaddr_in client;
+int main() {
+    char buff[1000], input[100];
+    int k, sock_desc;
+    struct sockaddr_in client;
 
-	sock_desc = socket(AF_INET, SOCK_STREAM, 0);
-	if(sock_desc < 0)
-	{
-		printf("\nSocket Creation Failed\n");
-		return -1;
-	}
+    sock_desc = socket(AF_INET, SOCK_STREAM, 0);
+    if (sock_desc < 0) {
+        printf("\nSocket Creation Failed\n");
+        return -1;
+    }
+    printf("\nSocket Created\n");
 
-	printf("\nSocket Created\n");
+    client.sin_family = AF_INET;
+    client.sin_port = htons(4000);
+    client.sin_addr.s_addr = INADDR_ANY;
 
-	//config.
-	client.sin_port = 4000;
-	client.sin_family = AF_INET;
-	client.sin_addr.s_addr = INADDR_ANY;
+    k = connect(sock_desc, (struct sockaddr*)&client, sizeof(client));
+    if (k < 0) {
+        printf("\nConnection Failed!\n");
+        return -1;
+    }
+    printf("\nConnection Success\n");
 
-	//connect the server.
-	k = connect(sock_desc, (struct sockaddr*) &client, sizeof(client));
-	if(k , 0)
-	{
-		printf("\nConnection Failed !\n");
-		return -1;
-	}
-
-	printf("\nConnection Success\n");
-
-	//data read.
-	// printf("Enter the Number : ");
-	// fgets(buff, 100, stdin);
-	bzero(buff, sizeof(buff));
-	strcpy(buff, "HI");
-
-	//send HI.
-	k = send(sock_desc, buff, 100, 0);
-	if(k , 0)
-	{
-		printf("\nData Sent Failed !\n");
-		return -1;
-	}
-
-	//recv from server.
-	bzero(buff, sizeof(buff));
-    read(sock_desc, buff, sizeof(buff));
-    if(strncmp(buff, "220", 3) == 0)
-    {
-    	printf("\nmsg from server %s\n", buff);
+    strcpy(buff, "HI");
+    k = send(sock_desc, buff, strlen(buff), 0);
+    if (k < 0) {
+        printf("\nData Sent Failed!\n");
+        return -1;
     }
 
-    //HELLO.
     bzero(buff, sizeof(buff));
-	strcpy(buff, "HELLO");
-	//send HELLO.
-	k = send(sock_desc, buff, 100, 0);
-	if(k < 0)
-	{
-		printf("\nData Sent Failed !\n");
-		return -1;
-	}
-
-    //recv from sever HELLO response.
     read(sock_desc, buff, sizeof(buff));
-    if(strncmp(buff, "250 OK", 6) == 0)
-    {
-    	printf("\nmsg from server %s\n", buff);
+    if (strncmp(buff, "220", 3) == 0) {
+        printf("\nMsg from server: %s\n", buff);
     }
 
-    //MAILFROM.
+    strcpy(buff, "HELLO");
+    send(sock_desc, buff, strlen(buff), 0);
+
     bzero(buff, sizeof(buff));
-	strcpy(buff, "MAILFROM abc@gmail.com");
-	//send MAILFROM.
-	k = send(sock_desc, buff, 100, 0);
-	if(k , 0)
-	{
-		printf("\nData Sent Failed !\n");
-		return -1;
-	}
-
-	//recv from sever MAILFROM response.
-	bzero(buff, sizeof(buff));
     read(sock_desc, buff, sizeof(buff));
-    if(strncmp(buff, "250 OK", 6) == 0)
-    {
-    	printf("\nmsg from server %s\n", buff);
+    if (strncmp(buff, "250 OK", 6) == 0) {
+        printf("\nMsg from server: %s\n", buff);
     }
 
-    //MAILTO.
+    printf("Enter MAILFROM address: ");
+    fgets(input, sizeof(input), stdin);
+    input[strcspn(input, "\n")] = 0;
+    snprintf(buff, sizeof(buff), "MAILFROM %s", input);
+    send(sock_desc, buff, strlen(buff), 0);
+
     bzero(buff, sizeof(buff));
-	strcpy(buff, "MAILTO bcd@gmail.com");
-	//send MAILTO.
-	k = send(sock_desc, buff, 100, 0);
-	if(k , 0)
-	{
-		printf("\nData Sent Failed !\n");
-		return -1;
-	}
-
-	//recv from sever MAILTO response.
-	bzero(buff, sizeof(buff));
     read(sock_desc, buff, sizeof(buff));
-    if(strncmp(buff, "250 OK", 6) == 0)
-    {
-    	printf("\nMAILTO response from server %s\n", buff);
+    if (strncmp(buff, "250 OK", 6) == 0) {
+        printf("\nMsg from server: %s\n", buff);
     }
 
-    //DATA
+    printf("Enter MAILTO address: ");
+    fgets(input, sizeof(input), stdin);
+    input[strcspn(input, "\n")] = 0;
+    snprintf(buff, sizeof(buff), "MAILTO %s", input);
+    send(sock_desc, buff, strlen(buff), 0);
+
     bzero(buff, sizeof(buff));
-	strcpy(buff, "DATA");
-	//send DATA.
-	k = send(sock_desc, buff, 100, 0);
-	if(k , 0)
-	{
-		printf("\nData Sent Failed !\n");
-		return -1;
-	}
-
-	//recv from sever DATA response.
-	bzero(buff, sizeof(buff));
     read(sock_desc, buff, sizeof(buff));
-    if(strncmp(buff, "354", 3) == 0)
-    {	
-    	printf("\nDATA response from server %s\n", buff);
+    if (strncmp(buff, "250 OK", 6) == 0) {
+        printf("\nMAILTO response from server: %s\n", buff);
     }
 
+    strcpy(buff, "DATA");
+    send(sock_desc, buff, strlen(buff), 0);
 
-    //MAILBODY
     bzero(buff, sizeof(buff));
-	strcpy(buff, "MAILBODY");
-
-	//from 9th index onwards type the body.
-	while (1) {
-        printf("Enter a line (or type 'END' to finish): ");
-        fgets(buff, sizeof(buff), stdin); // Read a line of input
-
-        // Remove the newline character at the end of the input, if it exists
-        buff[strcspn(buff, "\n")] = '\0';
-
-        // Check if the input matches the delimiter
-        if (strcmp(buff, "END") == 0) {
-            break; // Exit the loop if the delimiter is entered
-        }
-    }
-
-	//send BODY.
-	k = send(sock_desc, buff, 100, 0);
-	if(k , 0)
-	{
-		printf("\nData Sent Failed !\n");
-		return -1;
-	}
-
-	//recv from sever DATA response.
-	bzero(buff, sizeof(buff));
     read(sock_desc, buff, sizeof(buff));
-    if(strncmp(buff, "221", 3) == 0)
-    {
-    	printf("\nmsg from server %s\n", buff);
+    if (strncmp(buff, "354", 3) == 0) {
+        printf("\nDATA response from server: %s\n", buff);
     }
 
-    printf("\nEnd");
+    printf("Enter email body (type 'END' to finish):\n");
+    bzero(buff, sizeof(buff));
+    while (1) {
+        fgets(input, sizeof(input), stdin);
+        input[strcspn(input, "\n")] = 0;
+        if (strcmp(input, "END") == 0) break;
+        strcat(buff, input);
+        strcat(buff, "\n");
+    }
 
-	return 0;
+    send(sock_desc, buff, strlen(buff), 0);
+
+    strcpy(buff, "END");
+    send(sock_desc, buff, strlen(buff), 0);
+
+    bzero(buff, sizeof(buff));
+    read(sock_desc, buff, sizeof(buff));
+    if (strncmp(buff, "221", 3) == 0) {
+        printf("\nMsg from server: %s\n", buff);
+    }
+
+    close(sock_desc);
+    printf("\n__Qutting__\n");
+    return 0;
 }
+
+OUTPUT
+------
+s6@ubuntu:/alwin:$ ./client 
+
+Socket Created
+
+Connection Success
+
+Msg from server: 220
+
+Msg from server: 250 OK
+Enter MAILFROM address: abc@gmail.com
+
+Msg from server: 250 OK
+Enter MAILTO address: bcd@gmail.com
+
+MAILTO response from server: 250 OK
+
+DATA response from server: 354
+Enter email body (type 'END' to finish):
+Hi
+Hello
+END
+
+Msg from server: 221
+
+__Qutting__
--- smtp/server.c
+++ smtp/server.c
@@ -4,165 +4,122 @@
 #include <sys/socket.h>
 #include <netinet/in.h>
 #include <unistd.h>
+#include <sys/types.h>
 
-int main()
-{
-	char buff[100];
-	socklen_t len;
-	int k , sock_desc, temp_sock_desc;
-	struct sockaddr_in server, client;
+int main() {
+    char buff[100];
+    socklen_t len;
+    int k, sock_desc, temp_sock_desc;
+    struct sockaddr_in server, client;
 
-	//creating socket.
-	sock_desc = socket(AF_INET, SOCK_STREAM, 0);
-	//check the status of creation.
-	if(sock_desc < 0)
-	{
-		printf("\nSocket Creation Failed!\n");
-		return -1;
-	}
+    sock_desc = socket(AF_INET, SOCK_STREAM, 0);
+    if (sock_desc < 0) {
+        printf("\nSocket Creation Failed!\n");
+        return -1;
+    }
+    printf("\nSocket Created!\n");
 
-	printf("\nSocket Creation Success!\n");
+    server.sin_family = AF_INET;
+    server.sin_port = htons(4000);
+    server.sin_addr.s_addr = INADDR_ANY;
 
-	//configure the server socket.
-	server.sin_family = AF_INET; //ipv4 
-	server.sin_port = 4000; //port
-	server.sin_addr.s_addr = INADDR_ANY; //ip of this machine.
+    k = bind(sock_desc, (struct sockaddr*)&server, sizeof(server));
+    if (k < 0) {
+        printf("\nSocket Bind Failed!\n");
+        return -1;
+    }
+    printf("\nSocket Bind Success\n");
 
+    listen(sock_desc, 5);
+    printf("\nListening for connections...\n");
 
-	//configure client socket.
-	client.sin_family = AF_INET; //ipv4.
-	client.sin_port = 4000; //port.
-	client.sin_addr.s_addr = INADDR_ANY; //ip of the machine.
+    len = sizeof(client);
+    temp_sock_desc = accept(sock_desc, (struct sockaddr*)&client, &len);
+    if (temp_sock_desc < 0) {
+        printf("\nAccept Connection Failed!\n");
+        return -1;
+    }
+    printf("\nAccepted Connection\n");
 
-	//bind the socket and the server(config).
-	k = bind(sock_desc, (struct sockaddr *) &server, sizeof(server));
-	if(k < 0)
-	{
-		printf("\nSocket Bind Failed !\n");
-		return -1;
-	}
-
-	printf("\nSocket Bind Success\n");
-
-	//listen for connections.
-	k = listen(sock_desc, 5);
-	if(k < 0)
-	{
-		printf("\nListening For Connections Failed !\n");
-		return -1;
-	}
-
-	printf("\nListening for connections\n");
-
-	//accepting client connections.
-	len = sizeof(client);
-	temp_sock_desc = accept(sock_desc, (struct sockaddr*) &client, &len);
-	if(temp_sock_desc < 0)
-	{
-		printf("\nAccept Connetion Failed !\n");
-		return -1;
-	}
-
-	printf("\nStarted Accepting Connections\n");
-
-	//recv data from the client.
-	k = recv(temp_sock_desc, buff, 100, 0);
-	if( k < 0)
-	{
-		printf("\nData Recv Failed !\n");
-		return -1;
-	}
-
-	//recv HI
-	printf("msg from client : %s", buff);
-	//clear buffer.
-	buff[0] = '\0';
-	// and send that buffer to client
-	strcpy(buff, "220");//220 plus ip. 
+    read(temp_sock_desc, buff, sizeof(buff));
+    printf("\nMsg from client: %s\n", buff);
+    strcpy(buff, "220");
     write(temp_sock_desc, buff, sizeof(buff));
 
-    //recv HELLO
-    k = recv(temp_sock_desc, buff, 100, 0);
-	if( k < 0)
-	{
-		printf("\nData Recv Failed !\n");
-		return -1;
-	}
+    read(temp_sock_desc, buff, sizeof(buff));
+    if (strncmp(buff, "HELLO", 5) == 0) {
+        printf("\nMsg from client: %s\n", buff);
+        strcpy(buff, "250 OK");
+        write(temp_sock_desc, buff, sizeof(buff));
+    }
 
-	printf("\nmsg from client : %s", buff);
-	//clear buffer.
-	buff[0] = '\0';
-	// and send that buffer to client
-	strcpy(buff, "250 OK");//220 plus ip. 
+    read(temp_sock_desc, buff, sizeof(buff));
+    if (strncmp(buff, "MAILFROM", 8) == 0) {
+        printf("\nMsg from client: %s\n", buff);
+        strcpy(buff, "250 OK");
+        write(temp_sock_desc, buff, sizeof(buff));
+    }
+
+    read(temp_sock_desc, buff, sizeof(buff));
+    if (strncmp(buff, "MAILTO", 6) == 0) {
+        printf("\nMsg from client: %s\n", buff);
+        strcpy(buff, "250 OK");
+        write(temp_sock_desc, buff, sizeof(buff));
+    }
+
+    read(temp_sock_desc, buff, sizeof(buff));
+    if (strncmp(buff, "DATA", 4) == 0) {
+        printf("\nMsg from client: %s\n", buff);
+        strcpy(buff, "354");
+        write(temp_sock_desc, buff, sizeof(buff));
+    }
+
+    printf("\nEmail Body:\n");
+	bzero(buff, sizeof(buff));
+    read(temp_sock_desc, buff, sizeof(buff));
+	printf("\n---------------------------------\n");
+    printf("%s\n", buff);
+	printf("\n---------------------------------\n");
+
+    strcpy(buff, "221");
     write(temp_sock_desc, buff, sizeof(buff));
 
+	printf("__Quitting__");
 
-    //recv MAILFROM
-    k = recv(temp_sock_desc, buff, 100, 0);
-	if( k < 0)
-	{
-		printf("\nData Recv Failed !\n");
-		return -1;
-	}
-
-	printf("\nmsg from client : %s", buff);
-	//clear buffer.
-	buff[0] = '\0';
-	// and send that buffer to client
-	strcpy(buff, "250 OK"); 
-    write(temp_sock_desc, buff, sizeof(buff));
-
-    //recv MAILTO
-    k = recv(temp_sock_desc, buff, 100, 0);
-	if( k < 0)
-	{
-		printf("\nData Recv Failed !\n");
-		return -1;
-	}
-
-	printf("\nmsg from client : %s", buff);
-	//clear buffer.
-	buff[0] = '\0';
-	// and send that buffer to client
-	strcpy(buff, "250 OK"); 
-    write(temp_sock_desc, buff, sizeof(buff));
-
-    //recv DATA
-    buff[0] = '\0';
-    k = recv(temp_sock_desc, buff, 100, 0);
-	if( k < 0)
-	{
-		printf("\nData Recv Failed !\n");
-		return -1;
-	}
-	
-	printf("\nmsg from client : %s", buff);
-	//clear buffer.
-	buff[0] = '\0';
-	// and send that buffer to client
-	strcpy(buff, "354"); 
-    write(temp_sock_desc, buff, sizeof(buff));
-
-    //MAILBODY
-    buff[0] = '\0';
-    k = recv(temp_sock_desc, buff, 100, 0);
-	if( k < 0)
-	{
-		printf("\nData Recv Failed !\n");
-		return -1;
-	}
-
-	if(strncmp(buff, "MAILBODY", 8) == 0)
-		printf("\nmsg from client : %s", buff);
-
-	//clear buffer.
-	buff[0] = '\0';
-	// and send that buffer to client
-	strcpy(buff, "221"); 
-    write(temp_sock_desc, buff, sizeof(buff));
-
-	close(temp_sock_desc);
-	close(sock_desc);
-
-	return 0;
+    close(temp_sock_desc);
+    close(sock_desc);
+    return 0;
 }
+
+OUTPUT
+------
+
+s6@ubuntu:/alwin:$ ./server 
+
+Socket Created!
+
+Socket Bind Success
+
+Listening for connections...
+
+Accepted Connection
+
+Msg from client: HI�
+
+Msg from client: HELLO
+
+Msg from client: MAILFROM abc@gmail.com
+
+Msg from client: MAILTO bcd@gmail.comom
+
+Msg from client: DATAOK
+
+Email Body:
+
+---------------------------------
+Hi
+Hello
+
+
+---------------------------------
+__Quitting__
