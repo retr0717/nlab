da24acb79503814ad16d7d8ec2f623c8d5d5cf0f
retr0717 <alwinaji717@gmail.com>
2025-03-16 13:18:00 +0530

    leaky bucket

--- leaky-bucket.c
+++ leaky-bucket.c
@@ -1,27 +1,43 @@
 #include <stdio.h>
 
-int main()
-{
-    int queries, storage, out_pkt_size = 0;
-    int in_pkt_size, bucket_size, remaining = 0;
+#define BUCKET_SIZE 10
+#define OUT_PKT_SIZE 1
 
-    storage = 0; // init packets in the bucket.
-    queries = 4; // no of time bucket content is checked.
-    bucket_size = 10; // tot no of packets a bucket can accommodate.
-    in_pkt_size = 4;// no packets enters the bucket at a time.
-    out_pkt_size = 1;// no packets exits the bucket at a time.
-
-    for(int i = 0 ; i < queries ; i++)
-    {
-        remaining = bucket_size - storage;
-        if(in_pkt_size <= remaining)
-            storage += in_pkt_size;
-        else
-            printf("\nPacket Loss!! %d\n", in_pkt_size);
-
-        printf("\nBufer Size = %d \nOutput Bucket Size = %d\n", storage, bucket_size);
-        storage -= out_pkt_size;
+int main() {
+    int storage = 0; // Initial packets in the bucket
+    int queries;
+    
+    // Read the number of queries from the user
+    printf("Enter the number of queries: ");
+    scanf("%d", &queries);
+    
+    int packets[queries]; // Dynamically allocate array size based on user input
+    
+    // Read packet sizes from the user
+    printf("Enter %d incoming packet sizes: ", queries);
+    for (int i = 0; i < queries; i++) {
+        scanf("%d", &packets[i]);
     }
 
+    for (int i = 0; i < queries; i++) {
+        int in_pkt_size = packets[i]; // Get the current incoming packet size
+        int remaining = BUCKET_SIZE - storage;
+        
+        if (in_pkt_size <= remaining) {
+            storage += in_pkt_size;
+        } else {
+            printf("\nPacket Loss!! %d\n", in_pkt_size - remaining);
+            storage = BUCKET_SIZE; // Bucket is full
+        }
+        
+        printf("\nBuffer Size = %d \nOutput Bucket Size = %d\n", storage, BUCKET_SIZE);
+        
+        if (storage >= OUT_PKT_SIZE) {
+            storage -= OUT_PKT_SIZE;
+        } else {
+            storage = 0; // Prevent negative storage
+        }
+    }
+    
     return 0;
 }
